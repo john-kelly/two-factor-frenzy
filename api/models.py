7f2cf0bdf4cd699bb98c4bf7b259b8dbdc7f1611
john kelly <johnkelly@codehs.com>
2015-06-06 15:21:16 -0700

    small changes to models

--- api/models.py
+++ api/models.py
@@ -3,13 +3,14 @@ from django.db import models
 class Organization(models.Model):
     """ Model for Organization. """
     name = models.CharField(max_length=256)
-    website = models.URLField()
-    logo = models.URLField()
-    twitter_handle = models.CharField(max_length=256)
+    website = models.URLField(blank=True, null=True)
+    logo = models.URLField(blank=True, null=True)
+    twitter_handle = models.CharField(max_length=256, blank=True,
+                                      null=True)
 
 class MFASupport(models.Model):
     """ Model for Multi-factor Authentication support."""
-    documentation = models.URLField()
+    documentation = models.URLField(blank=True, null=True)
     organization = models.OneToOneField(Organization)
     sms = models.BooleanField(default=False)
     phone_call = models.BooleanField(default=False)
