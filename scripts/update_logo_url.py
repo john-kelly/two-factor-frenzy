6d444fc04bbd4d2525b04f48ddffd3f5015ac276
Kurt Hepler <kurt@codehs.com>
2015-06-07 11:58:41 -0700

    redo organization logo urls

--- scripts/update_logo_url.py
+++ scripts/update_logo_url.py
@@ -7,19 +7,39 @@ Usage
 
 """
 
-
+import os
+import yaml
 from api.models import *
 
 
 def run():
     """Entry point for script."""
 
-    orgs = Organization.objects.all()
+    for fn in os.listdir('_data/'):
+        # if it is a yaml file
+        if fn.endswith('.yml') and fn != "main.yml" and fn != "providers.yml":
+            # get the filename to serve as the organization tag
+            filename = os.path.splitext(fn)
+            tag = filename[0]
+            print tag
+            # open the file for processing
+            with open('_data/' + fn, 'r') as content_file:
+                # load the yaml into the generator, create a list of objects
+                content = content_file.read()
+                generator = yaml.load_all(content)
+                list_content = list(generator)
+                index = list_content[0].keys()[0]
+                list_objects = list_content[0][index]
 
-    for org in orgs:
-        # correct the site logo
-        logo = org.logo
-        new_logo_base = 'https://twofactorauth.org/img/' + org.category + '/'
-        fixed_logo = logo.replace('https://twofactorauth.org/img/', new_logo_base)
-        org.logo = fixed_logo
-        org.save()
+                for org in list_objects:
+                    website = org['url']
+                    try:
+                        img = 'https://twofactorauth.org/img/' + tag + '/' + org['img']
+                    except:
+                        img = 'https://twofactorauth.org/img/logo.png'
+                    print
+                    print org['name']
+                    print org['url']
+                    organization = Organization.objects.get(website=website)
+                    organization.logo = img
+                    organization.save()
